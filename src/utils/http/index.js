d7d96668db05fc48c56ca271a3002ffcd47bdd06
zzzre <kkkkk>
2025-07-16 17:50:52 +0800

    11

--- src/utils/http/index.js
+++ src/utils/http/index.js
@@ -28,76 +28,76 @@ class Http {
   // 请求拦截
   httpInterceptorsRequest() {
     Http.axiosInstance.interceptors.request.use(
-      config => {
-        NProgress.start();
-        // 发送请求前，可在此携带 token
-        // if (token) {
-        //   config.headers['token'] = token
-        // }
-        return config;
-      },
-      error => {
-        showFailToast(error.message);
-        return Promise.reject(error);
-      }
+        config => {
+          // NProgress.start();
+          // 发送请求前，可在此携带 token
+          // if (token) {
+          //   config.headers['token'] = token
+          // }
+          return config;
+        },
+        error => {
+          showFailToast(error.message);
+          return Promise.reject(error);
+        }
     );
   }
 
   // 响应拦截
   httpInterceptorsResponse() {
     Http.axiosInstance.interceptors.response.use(
-      response => {
-        NProgress.done();
-        // 与后端协定的返回字段
-        return response.data;
-      },
-      error => {
-        NProgress.done();
-        // 处理 HTTP 网络错误
-        let message = "";
-        // HTTP 状态码
-        const status = error.response?.status;
-        switch (status) {
-          case 400:
-            message = "请求错误";
-            break;
-          case 401:
-            message = "未授权，请登录";
-            break;
-          case 403:
-            message = "拒绝访问";
-            break;
-          case 404:
-            message = `请求地址出错: ${error.response?.config?.url}`;
-            break;
-          case 408:
-            message = "请求超时";
-            break;
-          case 500:
-            message = "服务器内部错误";
-            break;
-          case 501:
-            message = "服务未实现";
-            break;
-          case 502:
-            message = "网关错误";
-            break;
-          case 503:
-            message = "服务不可用";
-            break;
-          case 504:
-            message = "网关超时";
-            break;
-          case 505:
-            message = "HTTP版本不受支持";
-            break;
-          default:
-            message = "网络连接故障";
-        }
+        response => {
+          // NProgress.done();
+          // 与后端协定的返回字段
+          return response.data;
+        },
+        error => {
+          // NProgress.done();
+          // 处理 HTTP 网络错误
+          let message = "";
+          // HTTP 状态码
+          const status = error.response?.status;
+          switch (status) {
+            case 400:
+              message = "请求错误";
+              break;
+            case 401:
+              message = "未授权，请登录";
+              break;
+            case 403:
+              message = "拒绝访问";
+              break;
+            case 404:
+              message = `请求地址出错: ${error.response?.config?.url}`;
+              break;
+            case 408:
+              message = "请求超时";
+              break;
+            case 500:
+              message = "服务器内部错误";
+              break;
+            case 501:
+              message = "服务未实现";
+              break;
+            case 502:
+              message = "网关错误";
+              break;
+            case 503:
+              message = "服务不可用";
+              break;
+            case 504:
+              message = "网关超时";
+              break;
+            case 505:
+              message = "HTTP版本不受支持";
+              break;
+            default:
+              message = "网络连接故障";
+          }
 
-        showFailToast(message);
-        return Promise.reject(error);
-      }
+          showFailToast(message);
+          return Promise.reject(error);
+        }
     );
   }
 
@@ -106,13 +106,13 @@ class Http {
     const config = { ...Http.axiosConfigDefault, ...paramConfig };
     return new Promise((resolve, reject) => {
       Http.axiosInstance
-        .request(config)
-        .then(response => {
-          resolve(response);
-        })
-        .catch(error => {
-          reject(error);
-        });
+          .request(config)
+          .then(response => {
+            resolve(response);
+          })
+          .catch(error => {
+            reject(error);
+          });
     });
   }
 }
